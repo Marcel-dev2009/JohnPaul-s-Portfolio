747f46be0b50de0caf4d991184f68b8f6fe1c2f3
Marcel <izuobasomtochukwu@gmail.com>
2025-08-31 14:54:19 +0100

    SEO config

--- portfolio/app/layout.tsx
+++ portfolio/app/layout.tsx
@@ -4,6 +4,9 @@ import { ThemeProvider } from "next-themes";
 export const metadata: Metadata = {
   title: "John Paul Nwanganga - NollyWood Movie Director",
   description: "The official portfolio of John Paul Nwanganga, a NollyWood movie director.",
+  verification :{
+    google : "xlwM-qGHkTCuDIkG-Zfap1Rih1r0XEeYoAAZIRZW3uk"
+  }
 };
 
 export default function RootLayout({
@@ -13,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning className="scroll-smooth">
+      
       <body className="duration-300 overflow-x-hidden">
         <ThemeProvider enableSystem={true} attribute="class" defaultTheme="system">
         {children} 
